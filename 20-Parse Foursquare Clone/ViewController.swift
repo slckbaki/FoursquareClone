bbc8e50070bf23d190b6462c42d14c2d9d7752e3
selcuk baki <selcukbaki@windowslive.com>
2021-09-04 14:09:06 +0300

    second sourcetree commit

--- 20-Parse Foursquare Clone/ViewController.swift
+++ 20-Parse Foursquare Clone/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         // First commit for github test
+        
+        //Second Commit for sourcetree
     }
 
 
