6b1b45bf3749341addcc357eaac48538c993474d
selcuk baki <selcukbaki@windowslive.com>
2021-09-04 14:07:39 +0300

    Revert "sourcetree delete commit"
    This reverts commit 13e9465332ff1a29b4fb5ad9123f056e8a9a3afd.

--- 20-Parse Foursquare Clone/ViewController.swift
+++ 20-Parse Foursquare Clone/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // First commit for github test
     }
 
 
